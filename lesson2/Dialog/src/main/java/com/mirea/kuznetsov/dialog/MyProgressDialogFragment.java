d52bbf9036c0754bf2f54b188846392e06749647
biostunt <nice.nikita99@mail.ru>
2021-12-04 22:50:43 +0300

    Fix ProgressDialog

--- lesson2/Dialog/src/main/java/com/mirea/kuznetsov/dialog/MyProgressDialogFragment.java
+++ lesson2/Dialog/src/main/java/com/mirea/kuznetsov/dialog/MyProgressDialogFragment.java
@@ -2,6 +2,7 @@ package com.mirea.kuznetsov.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -16,31 +17,14 @@ import androidx.fragment.app.DialogFragment;
 
 public class MyProgressDialogFragment extends DialogFragment {
 
-    protected ProgressBar progressBar;
-
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        //ProgressDialog is now Deprecated (API 26)
-        builder
-                .setTitle("Progress Bar")
-                .setView(R.layout.progress_bar)
-                .setPositiveButton("Ок", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        ((MainActivity)getActivity()).onReceiveProps(
-                                String.format("Current progress: ProgressDialog is now Deprecated (API 26)")
-                        );
-                        dialog.cancel();
-                    }
-                });
-
-        return builder.create();
+        return new ProgressDialog(getContext());
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        progressBar = view.findViewById(R.id.progressBar);
     }
 }
